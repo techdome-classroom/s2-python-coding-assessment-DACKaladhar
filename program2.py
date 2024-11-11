8495ff161562275d6bca6346d8581c362c8c19c2
Abhiram chandra kaladhar <119961729+DACKaladhar@users.noreply.github.com>
2024-11-11 15:40:33 +0530

    Update program2.py
    First answer after unsuccesful push

--- program2.py
+++ program2.py
@@ -1,3 +1,26 @@
 class Solution(object):
+    def __init__(self):
+        # define conversions here so that it can be used everywhere
+        self.conversions = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
     def romanToInt(self, s):
-        pass
+        total = 0
+        prevValue = 0
+        
+        for char in reversed(s):
+            value = self.conversions[char]
+            if value < prevValue:  # we must subtract in these cases IV- is the example.
+                total -= value
+            else:  # we can add example VI
+                total += value
+            prevValue = value
+        
+        return total
