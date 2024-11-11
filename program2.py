62e7f36b96d2cd579c2994ff0f76a6de3bce2939
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:07:40 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,15 +1,25 @@
 class Solution(object):
-    self.d = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
+    def __init__(self):
+        self.d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
     def romanToInt(self, s):
+        total = 0
+        prev_value = 0
         
-
-
+        for char in reversed(s):
+            value = self.d[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        
+        return total
