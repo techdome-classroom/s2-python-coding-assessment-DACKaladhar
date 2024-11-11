532319f30dc5b2f619c6ff28cb0d2130da0aba59
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:04:43 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+        stack = []
+        for e in s:
+            if e in '[{(':
+                stack.append(e)
+            else:
+                if not stack:
+                    return False
+                elif (e == ')' and stack[-1] != '(') or (e == ']' and stack[-1] != '[') or (e == '}' and stack[-1] != '{'):
+                    return False
+                else:
+                    stack.pop()
+        
+        if stack:
+            return False
+        return True
