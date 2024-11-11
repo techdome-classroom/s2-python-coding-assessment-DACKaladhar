a281198160f76571541791602dfc844bdd3e9f5b
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:14:53 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,15 +1,3 @@
 class Solution(object):
-    def __init__(self):
-        # define conversions here so that it can be used everywhere
-        self.conversions = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-
     def romanToInt(self, s):
         pass
