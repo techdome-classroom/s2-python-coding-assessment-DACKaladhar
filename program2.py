62f9f622383b7d4cb99d591f37b5a01e4ed512bf
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:07:16 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,11 +1,14 @@
 class Solution(object):
-    self.d = {I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000}
+    self.d = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
     def romanToInt(self, s):
         
 
