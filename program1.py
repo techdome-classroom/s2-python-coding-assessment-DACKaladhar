690cedf621e11816eb93af3296548e571d8dfac5
DACKaladhar <darck21042004@gmail.com>
2024-11-11 14:59:18 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,7 +2,7 @@ class Solution(object):
     def isValid(self, s):
         stack = []
         for e in s:
-            if e == '[':
+            if e in '[{(':
                 stack.append(e)
             elif 
 
